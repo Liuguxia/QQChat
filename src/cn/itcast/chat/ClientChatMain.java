f81af54acf5019ff0c15a55e6dfa3b9a3962e133
Liuguxia <1102356056@qq.com>
2018-09-15 20:20:59 +0800

    点击按钮实现发送功能，注意：第一版本的客户端的jts滚动条有误

--- src/cn/itcast/chat/ClientChatMain.java
+++ src/cn/itcast/chat/ClientChatMain.java
@@ -2,9 +2,13 @@ package cn.itcast.chat;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
 
 //客户端
-public class ClientChatMain extends JFrame{
+public class ClientChatMain extends JFrame implements ActionListener {
     public static void main(String[] args) {
         new ClientChatMain();
     }
@@ -14,16 +18,18 @@ public class ClientChatMain extends JFrame{
     private JPanel jp;
     private JTextField jtf;
     private JButton jb;
+    //输出流
+    private BufferedWriter bw=null;
 
     //初始化属性，使用构造方法
     public ClientChatMain(){
         jta=new JTextArea();
         //设置文本域不可编辑
         jta.setEditable(false);
-        jsp=new JScrollPane();
-        jsp.add(jta);
+        //版本一此处错误
+        jsp=new JScrollPane(jta);
         jp=new JPanel();
-        jtf=new JTextField(10);
+        jtf=new JTextField(15);
         jb=new JButton("发送");
         jp.add(jtf);
         jp.add(jb);
@@ -36,9 +42,46 @@ public class ClientChatMain extends JFrame{
         this.setLocation(600,300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        //给按钮绑定一个监听事件
+        jb.addActionListener(this);
+
+        /***************TCP客户端开始******************/
+        //1.创建客户端的套接字（尝试连接）
+        //2.获取socket通道的输入流
+        //3.收取socket通道的输出流
+        //4.关闭socket通道
+        /***************TCP客户端结束******************/
+        try {
+            Socket socket = new Socket("127.0.0.1", 8888);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String line=null;
+            while ((line=br.readLine())!=null){
+                jta.append(line+System.lineSeparator());
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String text=jtf.getText();
+        text="服务端对客户端说："+text;
+        jta.append(text+System.lineSeparator());
+        try {
+            //发送
+            bw.write(text);
+            bw.newLine();
+            bw.flush();
+            jtf.setText("");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
     //行为
 
 }
--- src/cn/itcast/chat/ServerChatMain.java
+++ src/cn/itcast/chat/ServerChatMain.java
@@ -2,9 +2,15 @@ package cn.itcast.chat;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 //服务端
-public class ServerChatMain extends JFrame{
+//使用网络编程实现数据的传输（TCP）
+public class ServerChatMain extends JFrame implements ActionListener {
     //主方法
     public static void main(String[] args) {
         new ServerChatMain();
@@ -15,6 +21,8 @@ public class ServerChatMain extends JFrame{
     private JPanel jp;
     private JTextField jtf;
     private JButton jb;
+    //输出流
+    private BufferedWriter bw=null;
 
     //初始化属性，使用构造方法
     public ServerChatMain(){
@@ -41,6 +49,58 @@ public class ServerChatMain extends JFrame{
         this.setLocation(300,300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        //给发送按钮绑定一个监听点击事件
+        jb.addActionListener(this);
+
+        /***************TCP服务端开始******************/
+        //1.创建服务端的套接字
+        //2.等待客户端连接
+        //3.获取socket通道的输入流
+        //4.获取socket通道的输出流
+        //5.关闭socket通道
+        /***************TCP服务端结束******************/
+
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            Socket socket=serverSocket.accept();
+            //3.获取socket通道的输入流(输入流实现读取数据，一行一行读)
+            //InputStream in=socket.getInputStream();
+            BufferedReader br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bw=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String line=null;
+            while ((line=br.readLine())!=null){
+                //将读取的数据拼接到文本域中显示
+                jta.append(line+System.lineSeparator());
+            }
+            //4.获取socket通道的输出流（输出流写出数据）
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //监听事件
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //System.out.println("发送按钮被点击了");
+        //1.获取文本框发送的的内容
+        String text=jtf.getText();
+        //2.拼接收据内容
+        text="服务端对客户端说："+text;
+        //3.自己也需要显示
+        jta.append(text+System.lineSeparator());
+        //4.发送
+        try {
+            bw.write(text);//写进去
+            bw.newLine();//换行
+            bw.flush();//刷新
+            //5.清空文本框内容
+            jtf.setText("");
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
     //行为
 }
